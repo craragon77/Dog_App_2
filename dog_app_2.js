10c153da1643e1913d36073114f0c4ad716bf9f4
Chris Aragon <aragonchristopherray@gmail.com>
2020-01-15 16:58:07 -0500

    this is a tough one

--- dog_app_2.js
+++ dog_app_2.js
@@ -13,27 +13,34 @@ function fetchBreedList() {
 function listOptions(responseJson){
     const breeds = Object.keys(responseJson.message)
     const subBreeds = Object.values(responseJson.message)
-    for (let i = 0; i < subBreeds.length; i++){
-        if (subBreeds[i].length > 0 ){
-            console.log(subBreeds[i])
-        }
-    }
     for (let i = 0; i < breeds.length; i++){
-        $(".breed_list").append(
-            `<option>${breeds[i]}</option>`
-        )
+        if (subBreeds.length >= 1 ){
+            for (let i = 0; i < subBreeds.length; i++){
+                $(".breed_list").append(
+                `<option>${subBreeds[i]}  ${breeds[i]}</option>`
+                )
+            }
+        }
+        else {
+            $(".breed_list").append(
+                `<option>${breeds[i]}</options`
+            )
+        }
     }
 }
 
-function genList(){
-    $(".make_breed").click(function(){
-        alert("hello, Dave")
+/*function genPic(){
+    $(".make_breed").click(function(event){
+        event.preventDefault();
+        fetchDogPicture()
+        $("")
     })
-}
+} */
 
 function valueArray(responseJson) {
     console.log(responseJson.message)
 }
 
 fetchBreedList()
-genList()
+//fetchDogPicture()
+//genPic()
