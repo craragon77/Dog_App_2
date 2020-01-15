e1b367f59b2717c9f192d900975cd911ca2528b6
Chris Aragon <aragonchristopherray@gmail.com>
2020-01-15 11:38:22 -0500

    first commit

---
+++ dog_app_2.js
@@ -0,0 +1,32 @@
+
+function fetchBreedList() {
+    fetch('https://dog.ceo/api/breeds/list/all')
+    .then(response => response.json())
+    .then(responseJson => console.warn(responseJson) && listOptions(responseJson) && valueArray(responseJson))
+    .catch(warning => console.warn(warning) || alert("Ruh-Roh! Something went wrong! Please try again later"))
+}
+
+function listOptions(responseJson){
+    for (let i = 0; i < responseJson.message.length; i++){
+        $("option").replaceWith(
+            `<option>${responseJson.message[i]}</option>`
+        )
+    }
+}
+
+function genList(){
+    $(".make_breed").click(function(event){
+        alert("hello, Dave")
+    })
+}
+
+function valueArray(responseJson) {
+    console.log(responseJson)
+}
+
+
+
+
+fetchBreedList()
+genList()
+valueArray()
