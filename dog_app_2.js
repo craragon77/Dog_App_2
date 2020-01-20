53cf68bb180e63c371f01fc095274d0a0e09a6b2
Chris Aragon <aragonchristopherray@gmail.com>
2020-01-20 15:00:32 -0500

    edits after feedback; now you can type the breed name

--- dog_app_2.js
+++ dog_app_2.js
@@ -1,67 +1,22 @@
-
-function fetchBreedList() {
-    fetch('https://dog.ceo/api/breeds/list/all')
-    .then(response => response.json())
-    .then(responseJson => {
-        console.warn(responseJson);
-        listOptions(responseJson);
-        valueArray(responseJson);
-    })
-    .catch(warning => console.warn(warning) || alert("Ruh-Roh! Something went wrong! Please try again later"))
-}
-function fetchDogPicture(dogArray) {
-    if (dogArray.length > 1){
-        console.log(dogArray)
-    fetch('https://dog.ceo/api/breed/' + dogArray[1] + '/' + dogArray[0] +'/images/random')
-    .then(response => response.json())
-    .then(doggyResponseJson =>{
-        console.warn(doggyResponseJson);
-        genPic(doggyResponseJson);
-    })
-    .catch(error => console.warn(error)|| alert("Zoinks! We can't find any dogs of that breed! Please come back later"))
-    } else{
-        fetch('https://dog.ceo/api/breed/' + dogArray + '/images/random')
-        .then(response => response.json())
-        .then(doggyResponseJson =>{
-            console.warn(doggyResponseJson);
-            genPic(doggyResponseJson);
-        })
-        .catch(error => console.warn(error)|| alert("Zoinks! We can't find any dogs of that breed! Please come back later"))
-    }
-}
-
-
-function listOptions(responseJson){
-    const breeds = Object.keys(responseJson.message)
-    const subBreeds = Object.values(responseJson.message)
-    for (let i = 0; i < breeds.length; i++){
-        if (subBreeds[i].length >= 1 ){
-            for (let x = 0; x < subBreeds[i].length; x++){
-                $(".breed_list").append(
-                `<option>${subBreeds[i][x]} ${breeds[i]}</option>`
-                )
-            }
-        }
-        else {
-            $(".breed_list").append(
-                `<option>${breeds[i]}</options`
-            )
-        }
-    }
-    submitBreedButton()
+function fetchRequest(breed){
+    fetch('https://dog.ceo/api/breed/' + breed + '/images/random')
+    .then( results => results.json())
+    .then(doggyresponseJson => genPic(doggyresponseJson))
+    .catch(error => alert("Ruh-Roh! We can't find that breed! Please try a new one!"));
 }
 
 function submitBreedButton() {
-    $(".make_breed").on("click submit", (function(event){
+    $(".make_breed").click(function(event){
         event.preventDefault();
-        let stringBreed = $('select option:selected').text()
-        let dogArray = stringBreed.split(" ")
-        fetchDogPicture(dogArray)
+        userInput()
     })
-    )
 }
 
-
+function userInput(){
+    let breed = $(".breed_input").val()
+    console.log(breed)
+    fetchRequest(breed)
+}
 
 function genPic(doggyResponseJson){
     $(".dog_pic").replaceWith(
@@ -70,11 +25,5 @@ function genPic(doggyResponseJson){
         </section>`
     )
 }
-
-function valueArray(responseJson) {
-    console.log(responseJson.message)
-}
-
-fetchBreedList()
-
+submitBreedButton()
 
