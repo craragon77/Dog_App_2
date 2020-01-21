b61f4c83f4b2779a4730bb38235c54d1830f7688
Chris Aragon <aragonchristopherray@gmail.com>
2020-01-20 22:20:39 -0500

    finally works

--- dog_app_2.js
+++ dog_app_2.js
@@ -1,7 +1,13 @@
 function fetchRequest(breed){
     fetch('https://dog.ceo/api/breed/' + breed + '/images/random')
-    .then( results => results.json())
-    .then(doggyresponseJson => genPic(doggyresponseJson))
+    .then(results => results.json())
+    .then(doggyResponseJson => {
+        if (doggyResponseJson.status == 'success'){
+        genPic(doggyResponseJson)
+        } else {
+            throw doggyResponseJson 
+        }
+    })
     .catch(error => alert("Ruh-Roh! We can't find that breed! Please try a new one!"));
 }
 
@@ -21,9 +27,13 @@ function userInput(){
 function genPic(doggyResponseJson){
     $(".dog_pic").replaceWith(
         `<section class="make_breed">
-            <img class="dog_pic" src="${doggyResponseJson.message}" alt="doggy">
+            <img class="dog_pic" src="${doggyResponseJson.message}">
         </section>`
     )
+}
+
+function test(doggyResponseJson){
+    console.log(doggyResponseJson)
 }
 submitBreedButton()
 
